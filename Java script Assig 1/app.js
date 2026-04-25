f0d7705f2003138537dd80566ba670010972630f
Abdul111222 <158484369+Abdul111222@users.noreply.github.com>
2026-04-25 10:30:38 -0400

    Add files via upload

---
+++ Java script Assig 1/app.js
@@ -0,0 +1,83 @@
+// Javascript File Practice
+
+// stores city
+var city = "Karachi";
+
+// Pirnts Hello World 
+console.log("Hello World");
+// Prints my Name
+console.log('My name is "Abdul Qadeer" ');
+// Prints my Age
+console.log('I am "20" years old');
+// Prints the stored variable Name
+console.log("My city Name is " +city);
+
+// 3.Variables (let , const , var)
+let aNumber = 9;
+const fullName = "Abdul Qadeer";
+var c = 20;
+var age = c
+let d = 20;
+d = 21;
+console.log(d);
+
+
+// 4.primitive Data Types
+
+let myName = "Abdul Qadeer";
+let myAge= 30;
+let beingStudent = true;
+let notDefined;
+
+ // 5. Type of Operator
+ console.log(typeof "Hello world");
+ console.log(typeof 12);
+ console.log(typeof true);
+ console.log(typeof undefined);
+ console.log(typeof [1,2,3,4])
+
+ // 6. Arithimetic Operators
+
+ console.log(10 + 5);  // Addition: 15
+console.log(20 - 8);  // Subtraction: 12
+console.log(4 * 3);   // Multiplication: 12
+console.log(10 / 2);  // Division: 5
+console.log(10 % 3);  // Modulo (Remainder): 1
+ 
+// 7. Comaprison Operators
+
+console.log(5 == 5);     // true (Value checks)
+console.log(5 === "5");  // false (Checks value + type)
+console.log(10 > 5);     // true
+console.log(3 < 1);      // false
+
+// 8 Increment and decrement
+
+let x = 5;
+x++; // x becomes 6 (Post-increment)
+
+let y = 5;
+++y; // y becomes 6 (Pre-increment)
+
+let z = 10;
+--z; // z becomes 9
+
+// Difference Observation:
+let a = 5;
+console.log(a++); // Output: 5 (Print then increment)
+console.log(a);   // Output: 6
+
+let b = 5;
+console.log(++b); // Output: 6 (Increment then print)
+
+// Calculations
+
+// 2 variables numbers ke liye
+let num1 = 20;
+let num2 = 10;
+
+// Operations and Results
+console.log("Sum: " + (num1 + num2));
+console.log("Subtraction: " + (num1 - num2));
+console.log("Multiplication: " + (num1 * num2));
+console.log("Division: " + (num1 / num2));
